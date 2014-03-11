2df8b9aca29f2de6a057db34ac570e7bd923a4ae
Todd Kennedy <todd@selfassembled.org>
2014-03-10 21:02:41 -0400

    meh

--- expose-jquery.js
+++ expose-jquery.js
@@ -1,12 +1,4 @@
 'use strict';
 
-var $;
-
-if(!process.browser){
-  global.window = require('jsdom').jsdom('<html><head><title></title></head><body></body></html>').parentWindow;
-  $ = require('jquery');
-} else {
-  $ = window.$;
-}
-
-module.exports = $;
+global.window = require('jsdom').jsdom('<html><head><title></title></head><body></body></html>').parentWindow;
+module.exports = require('jquery');
--- test/rearview-client.js
+++ test/rearview-client.js
@@ -1,5 +1,20 @@
+/* global suite, test */
+
 'use strict';
 
-var test = require('tap').test;
-var rearview = require('../index');
+var assert = require('assert');
+var Rearview = require('../index');
 
+var TestView = Rearview.extend({
+  tmplFn: function(){
+    return 'this is a test';
+  }
+});
+
+suite('rendering');
+
+test('should render a template', function(){
+  var testView = new TestView();
+  var html = testView.render();
+  assert.equal(html, '<div>this is a test</div>', 'rendered server side');
+});
